9857d0916b1e9315f52358f1a60e61d71abbbe75
parvasdeoliveiraluana-art <parvasdeoliveiraluana@gmail.com>
2026-09-01 07:42:39 -0300

    Create app.js

---
+++ front-end/app.js
@@ -0,0 +1,15 @@
+function abrirFormulario(tipo) {
+
+    if (tipo === "espresso") {
+        window.location.href = "formularios/espresso.html";
+    }
+
+    else if (tipo === "uber") {
+        window.location.href = "formularios/uber.html";
+    }
+
+    else if (tipo === "webfrota") {
+        window.location.href = "formularios/webfrota.html";
+    }
+
+}
